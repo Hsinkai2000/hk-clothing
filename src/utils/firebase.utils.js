dd0610a2aee6f1174600f48bdcfa4202e6aaa7dc
Hsin Kai <45195644+Hsinkai2000@users.noreply.github.com>
2023-05-26 15:27:13 +0800

    Google Sign In

---
+++ src/utils/firebase.utils.js
@@ -0,0 +1,58 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import {
+    getAuth,
+    signInWithRedirect,
+    signInWithPopup,
+    GoogleAuthProvider,
+} from "firebase/auth";
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyB_lJSV2Zzo5DIVwZ0SHjIZs7capMRW1bY",
+    authDomain: "hk-clothing.firebaseapp.com",
+    projectId: "hk-clothing",
+    storageBucket: "hk-clothing.appspot.com",
+    messagingSenderId: "1094035138629",
+    appId: "1:1094035138629:web:9ee2b2e3920fbd6d805b2c",
+    measurementId: "G-TPC8QR466Y",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+const provider = new GoogleAuthProvider();
+provider.setCustomParameters({
+    prompt: "select_account",
+});
+
+export const auth = getAuth(app);
+
+export const signInWithGooglePopup = () => {
+    return signInWithPopup(auth, provider);
+};
+
+export const db = getFirestore();
+export const createUserDocumentFromAuth = async (userAuth) => {
+    const userDocRef = doc(db, "users", userAuth.uid);
+    console.log(userDocRef);
+    const userSnapshot = await getDoc(userDocRef);
+    if (!userSnapshot.exists()) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+
+        try {
+            await setDoc(userDocRef, { displayName, email, createdAt });
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    return userDocRef;
+};
