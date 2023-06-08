c40a60a9cb777d610283b7d0ce1f9bfe2ef41ecb
Hsin Kai <45195644+Hsinkai2000@users.noreply.github.com>
2023-06-08 15:31:49 +0800

    added in addToCart feature

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -3,11 +3,48 @@ import { useState, createContext, useEffect } from "react";
 export const CartContext = createContext({
     isCartOpen: false,
     setIsCartOpen: () => {},
+    cartItems: [],
+    addItemToCart: () => {},
+    cartCount: 0,
 });
+
+const addCartItem = (cartItems, productToAdd) => {
+    const existingItem = cartItems.find((item) => item.id === productToAdd.id);
+
+    if (existingItem) {
+        return cartItems.map((cartItem) =>
+            cartItem.id === productToAdd.id
+                ? { ...cartItem, quantity: cartItem.quantity + 1 }
+                : cartItem
+        );
+    }
+    return [...cartItems, { ...productToAdd, quantity: 1 }];
+};
 
 export const CartProvider = ({ children }) => {
     const [isCartOpen, setIsCartOpen] = useState(false);
-    const value = { isCartOpen, setIsCartOpen };
+    const [cartItems, setCartItems] = useState([]);
+    const [cartCount, setCartCount] = useState(0);
+
+    useEffect(() => {
+        const newCartCount = cartItems.reduce(
+            (acc, item) => acc + item.quantity,
+            0
+        );
+        setCartCount(newCartCount);
+    }, [cartItems]);
+
+    const addItemToCart = (productToAdd) => {
+        setCartItems(addCartItem(cartItems, productToAdd));
+    };
+
+    const value = {
+        isCartOpen,
+        setIsCartOpen,
+        addItemToCart,
+        cartItems,
+        cartCount,
+    };
     return (
         <CartContext.Provider value={value}>{children}</CartContext.Provider>
     );
--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -2,7 +2,6 @@ import { ProductContext } from "../../contexts/product.context";
 import { useContext } from "react";
 import ProductCard from "../../components/product-card/product-card.component";
 import "./shop.styles.scss";
-
 const Shop = () => {
     const { products } = useContext(ProductContext);
     return (
